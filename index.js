4d0b0938dd0f85a0b653696981396dd5aa5d0c66
Dharmesh_Rane <67591263+dharmesh15rane@users.noreply.github.com>
2023-08-04 12:43:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 const port = process.env.PORT || 3030;
 
-app.get('/', (req, res) =>res.send('Successfully deploy application in aws app-runner'))
+app.get('/', (req, res) =>  res.sendFile(__dirname + "/index.html"));
 
 app.listen(port, (err) => {
     if (err) {
